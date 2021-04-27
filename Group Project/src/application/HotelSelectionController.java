d7f723ab6dcb45c1613e7bb7f879dc8157f77cbc
gabygabyte <78988305+gabygabyte@users.noreply.github.com>
2021-04-26 20:00:54 -0500

    added methods: clickHome, hotelSelected, addHotel,  bookingScene, setHotel, clearHotels.
    only the clickHome method is complete.

--- Group Project/src/application/HotelSelectionController.java
+++ Group Project/src/application/HotelSelectionController.java
@@ -16,8 +16,48 @@ import javafx.stage.Stage;
  */
 
 public class HotelSelectionController {
-	
+	@FXML
+	private AnchorPane mainPane;
 
-
+	@FXML
+	public void clickHome(ActionEvent event) throws IOException {
+		
+		mainPane = FXMLLoader.load(getClass().getResource("Main.fxml")); //
+		Scene scene = new Scene(mainPane);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
 	
+	@FXML
+	public void hotelSelected(ActionEvent event) throws IOException {
+		
+	
+	}
+	
+	@FXML
+	public void addHotel(ActionEvent event) throws IOException {
+		
+	
+	}
+	
+	@FXML
+	public void bookingScene(ActionEvent event) throws IOException {
+		
+	
+	}
+	
+	
+	@FXML
+	public void setHotel(ActionEvent event) throws IOException {
+		
+	
+	}
+	
+	@FXML
+	public void clearHotels(ActionEvent event) throws IOException {
+		
+	
+	}
 }

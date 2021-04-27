d5843e3613506147519da1fc4ba08b5a8ae0d000
hannahb9011 <79071679+hannahb9011@users.noreply.github.com>
2021-04-27 16:07:06 -0500

    Add Search button

--- Group Project/src/application/EditingController.java
+++ Group Project/src/application/EditingController.java
@@ -1,19 +1,51 @@
 package application;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Properties;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class EditingController 
 {
+	private static HashMap<String, String> h = new HashMap<String, String>();
+	private static Properties properties = new Properties();//Create properties file to store hash map
+	
 	@FXML
 	private AnchorPane editingPanel;
+	
+	@FXML
+    private TextField checkinText;
+
+    @FXML
+    private TextField roomText;
+
+    @FXML
+    private TextField hotelText;
+
+    @FXML
+    private TextField childrenText;
+
+    @FXML
+    private TextField nameText;
+
+    @FXML
+    private TextField checkoutText;
+
+    @FXML
+    private TextField adultsText;
+    
+    @FXML
+    private TextField emailText;
 
 	@FXML
 	public void toHome(ActionEvent event) throws IOException {
@@ -24,6 +56,36 @@ public class EditingController
 		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 		window.setScene(scene);
 		window.show();
+	}
+	
+	public void Search(ActionEvent event) throws IOException  {
+		String compKey = nameText.getText().toString();
+		String info;
+        File file = new File("bookings.properties");//opens file
+        FileInputStream reader=new FileInputStream(file);//open reader
+        properties.load(reader);//loads/lets you see stored values
+        reader.close();
+
+        for(String key: properties.stringPropertyNames()){//adding values to hash map
+        	h.put(key, properties.get(key).toString());
+        }
+    	 if(h.containsKey(compKey)){//check if there's a key in the hash map that matches 
+ 	       	info = h.get(compKey); //set val to quantity of item
+         }
+    	 else {
+    		info = "Invalid name";//set val to show there's none in stock
+    	 }
+    	 
+    	 System.out.println(info);
+    	 
+    	 /*
+    	 hotelText.setText();
+    	 emailText.setText();
+    	 checkinText.setText();
+    	 checkoutText.setText();
+    	 roomText.setText();
+    	 adultsText.setText();
+    	 childrenText.setText();*/
 	}
 	
 	@FXML

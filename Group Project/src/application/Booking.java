c76a938583a7717db34a82b9ad6d17ef577814ca
Mark Zimmerschied <eyc259@my.utsa.edu>
2021-04-27 09:08:13 -0500

    Created Booking class for booking objects

---
+++ Group Project/src/application/Booking.java
@@ -0,0 +1,56 @@
+package application;
+
+public class Booking {
+	
+	String Hotels;
+	String Email;
+	String checkIn;
+	String CheckOut;
+	int NumRooms;
+	int NumAdults;
+	int NumChild;
+	
+	public String getHotels() {
+		return Hotels;
+	}
+	public void setHotels(String hotels) {
+		Hotels = hotels;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getCheckIn() {
+		return checkIn;
+	}
+	public void setCheckIn(String checkIn) {
+		this.checkIn = checkIn;
+	}
+	public String getCheckOut() {
+		return CheckOut;
+	}
+	public void setCheckOut(String checkOut) {
+		CheckOut = checkOut;
+	}
+	public int getNumRooms() {
+		return NumRooms;
+	}
+	public void setNumRooms(int numRooms) {
+		NumRooms = numRooms;
+	}
+	public int getNumAdults() {
+		return NumAdults;
+	}
+	public void setNumAdults(int numAdults) {
+		NumAdults = numAdults;
+	}
+	public int getNumChild() {
+		return NumChild;
+	}
+	public void setNumChild(int numChild) {
+		NumChild = numChild;
+	}
+	
+}

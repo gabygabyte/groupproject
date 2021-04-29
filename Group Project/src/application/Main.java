506c80a64ed79288cd7c3833a1974ad4b18a088d
hannahb9011 <79071679+hannahb9011@users.noreply.github.com>
2021-04-28 19:24:08 -0500

    fixed where it showed extra white space on main page

--- Group Project/src/application/Main.java
+++ Group Project/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,710,477);
+			Scene scene = new Scene(root,710,470);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
